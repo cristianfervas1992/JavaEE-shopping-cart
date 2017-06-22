0d553a41529429f6a9deeeb58cbff25100d08ec9
cristianfervas <cristianfervas1992@gmail.com>
2017-06-22 11:46:58 -0400

    Version 1.6
    Modificacion Test

--- test/com/ordermeal1/entities/Order1PKTest.java
+++ test/com/ordermeal1/entities/Order1PKTest.java
@@ -158,9 +158,9 @@ public class Order1PKTest {
         Order1PK instance = new Order1PK(1, 2, 3, 4, 5, 6, 7, 8, 9);
         int expResult = 8;
         int result = instance.getBebestibleIdBebestible();
-        assertEquals(expResult, result);
+        //assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        fail("The test case is a prototype.");
     }
 
     
